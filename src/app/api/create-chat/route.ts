618dc0e07370bb4f40784e3419268e4567b9442e
Ionel Miroslav <miroslav.ic27@gmail.com>
2025-01-21 20:24:57 -0800

    fix create-char POST api

--- src/app/api/create-chat/route.ts
+++ src/app/api/create-chat/route.ts
@@ -6,8 +6,8 @@ import { getS3Url } from "@/lib/s3"
 import { auth } from "@clerk/nextjs/server"
 import { NextResponse } from "next/server"
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function POST(req: Request, res: Response) {
+
+export async function POST(req: Request) {
     const {userId} = await auth()
     if (!userId) {
         return NextResponse.json({error: "unauthorized"}, {status: 401})
